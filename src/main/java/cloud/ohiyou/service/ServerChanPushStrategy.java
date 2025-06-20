1ed291bfa2b05b1b88455db1f19ff6eb4863a088
lgsp-Leslie <lgsp_leslie@yeah.net>
2025-06-20 15:16:21 +0800

    Update ServerChanPushStrategy.java

--- src/main/java/cloud/ohiyou/service/ServerChanPushStrategy.java
+++ src/main/java/cloud/ohiyou/service/ServerChanPushStrategy.java
@@ -32,6 +32,7 @@ public class ServerChanPushStrategy implements IMessagePushStrategy {
     public void pushMessage(String title, String message) {
         String token = EnvConfig.get().getServerChan();
         String u_id = EnvConfig.get().getServerChanUid();
+        System.out.println(u_id);
         String url   = null;
         try {
             url = "https://" + u_id + ".push.ft07.com/send/" + token + ".send?title=" +
